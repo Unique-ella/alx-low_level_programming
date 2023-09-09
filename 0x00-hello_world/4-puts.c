5182c9ec6a8b6b984e4b489f5fc4cde55c996aea
Unique-ella <nnaochaella@gmail.com>
2023-09-08 20:39:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Entry point - A program that will print a line of text with a new line
+ *
+ * Return : Always 0;
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
